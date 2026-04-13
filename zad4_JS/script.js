c9b82e285166a932fcdde6dd748e7ccc1bca3e57
Nikita75660 <nkorcha1@stu.vistula.edu.pl>
2026-04-13 18:27:16 +0200

    Update script.js

--- zad4_JS/script.js
+++ zad4_JS/script.js
@@ -21,3 +21,30 @@ function toggleProjects() {
         section.style.display = "none";
     }
 }
+function toggleUmiejętności() {
+    const section = document.getElementById("Umiejętności");
+
+    if (section.style.display === "none") {
+        section.style.display = "block";
+    } else {
+        section.style.display = "none";
+    }
+}
+function toggleDoświadczenie() {
+    const section = document.getElementById("Doświadczenie");
+
+    if (section.style.display === "none") {
+        section.style.display = "block";
+    } else {
+        section.style.display = "none";
+    }
+}
+function toggleEdukacja() {
+    const section = document.getElementById("Edukacja");
+
+    if (section.style.display === "none") {
+        section.style.display = "block";
+    } else {
+        section.style.display = "none";
+    }
+}
